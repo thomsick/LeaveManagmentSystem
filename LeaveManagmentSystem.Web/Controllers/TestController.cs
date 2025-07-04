f402625e2dce2b483f7ff113d2f17edff49d0c1e
Thomas Hartwig <thomas.hartwig44@icloud.com>
2025-07-04 07:24:51 +0200

    Add project files.

---
+++ LeaveManagmentSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagmentSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagmentSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Studento of MVS Mastary",
+                DateOfBirth = new DateTime(1990, 12, 13)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagmentSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagmentSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
